578588dfc67950862dc45c76a32afcec8c138558
ivy <unjust@gmail.com>
2018-10-08 00:14:41 -0500

    dynaic entry config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const glob = require('glob');
+
+// allows us to dynamically create file names
+const entryFiles = glob.sync('./js/**/*.*js');
+const entryConfig = entryFiles.reduce((config, item) => {
+    const filename = path.basename(item);
+    const name = filename.replace('.js', '');
+    config[name] = item;
+    return config;
+}, {});
 
 module.exports = {
     mode: 'development',
     watch: true,
-    entry: {
-        first: './js/first.js'
-    },
+    entry: entryConfig,
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: `[name].js`
