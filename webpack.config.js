170e853e8e2680924cb2f57b40917df4f70653b4
ivy <unjust@gmail.com>
2018-10-30 23:42:58 -0500

    ability to copy img assets, some date info

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const glob = require('glob');
+const fs = require('fs');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const entryFiles = glob.sync('./js/**/*.*js');
 
@@ -15,32 +18,43 @@ const entryConfig = entryFiles.reduce((config, item) => {
 const template = 'templates/template.html';
 const generateHtmlPluginCalls = () => {
     return Object.keys(entryConfig).map((entryName, index) => {
-       const config = {
+        const date = new Date(fs.statSync(entryFiles[index]).ctime);
+        const ctime = `${date.getDate()}\
+        -${date.getMonth() < 12 ? date.getMonth() + 1 : 12}\
+        -${date.getFullYear()}`;
+        const config = {
             chunks: [entryName],
             filename: `${entryName}.html`,
             template,
-            title: `${entryName} Paper`
+            title: `${entryName} Paper`,
+            ctime
         };
         return new HtmlWebpackPlugin(config);
     });
 };
+
+const buildPath = path.resolve(__dirname, 'build');
 
 module.exports = {
     mode: 'development',
     watch: true,
     entry: entryConfig,
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: buildPath,
         filename: `[name].js`
     },
-    plugins: generateHtmlPluginCalls(),
+    plugins: [
+        ...generateHtmlPluginCalls(),
+        new CopyWebpackPlugin([{ from: 'img', to: `${buildPath}/img` }])
+    ],
     module: {
         rules: [ 
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env']
+                    presets: ['@babel/preset-env'],
+                    sourceMap: true
                 }
             }
         ]
