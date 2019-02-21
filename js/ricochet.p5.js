4b0f987af471891914cf2bf62e584c431b594cb6
ivy <unjust@gmail.com>
2019-02-20 20:54:42 -0500

    trying to solve voector and hit proble,

--- js/ricochet.p5.js
+++ js/ricochet.p5.js
@@ -1,25 +1,34 @@
 import p5 from 'p5';
 
 new p5((sketch) => {
-    let start = p5.createVector(100, 100), 
-        end = p5.createVector(0, 0),
-        augment = p5.createVector(1, 1);
+    let start = sketch.createVector(100, 100), 
+        end = sketch.createVector(0, 0),
+        augment = sketch.createVector(1, 1);
 
-    const canvas_w = 1200, 
-        canvas_h = 800;
+    const distance = sketch.createVector(10, 10);
+    const canvas_w = 800, 
+        canvas_h = 600;
+
+    const hit = (v) => (v.x > canvas_w || v.x < 0 || v.y > canvas_h || v.y < 0);
 
     const update = () => {
         start.add(augment);
+        end.add(start - distance);
+        if (hit(start)) {
+            start.add(augment.mult(-1));
+        }
     };
 
     sketch.setup = () => {
-        p5.createCanvas(canvas_w, canvas_h, p5.WEBGL);
-        p5.fill('white');
+        const c = sketch.createCanvas(canvas_w, canvas_h, p5.WEBGL);
+        sketch._pixelDensity = 1;
+        c.parent('container');
+        sketch.fill('white');
     };
 
     sketch.draw = () => {
-        //sketch.clear();
+        sketch.clear();
         update();
-        p5.line(start.x, start.y, end.x, end.y);
+        sketch.line(start.x, start.y, end.x, end.y);
     };
 });
