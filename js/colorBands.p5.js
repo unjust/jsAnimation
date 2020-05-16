0c8ab45876981a1a105e9c74fad1aea6a14d916e
ivy <unjust@gmail.com>
2020-05-16 15:43:48 -0500

    color bands working with hsb better

--- js/colorBands.p5.js
+++ js/colorBands.p5.js
@@ -8,34 +8,48 @@ new p5((sk) => {
   let bandCount = 10;
   let canvasWidth, canvasHeight;
   let counter = 0;
+  let aColor, bColor;
 
   sk.setup = () => {
-    sk.createCanvas(400, 400, sk.WEBGL);
+    const cnv = sk.createCanvas(400, 400, sk.WEBGL);
     sk.windowResized();
     initBands();
     console.log('bands of color that breathe and change with mouse position');
+    cnv.mouseOver(() => console.log('overrrr'));
+    sk.colorMode(sk.HSB);
   };
 
+  const getMainColor = () => Math.ceil(sk.random(10, 255));
+
+  const getColorVariation = (mainHue) => `hsb(${mainHue}, ${Math.ceil(sk.random(10, 100))}%, ${Math.ceil(sk.random(10, 100))}%)`
+
   const initBands = () => {
+    // hsb(160, 100%, 50%)
+    const hue = getMainColor();
     colors = [...Array(bandCount + 1).keys()].map(() => {
-      return sk.color(sk.random(255), sk.random(255), sk.random(255), 255);
+      const hsb = getColorVariation(hue);
+      console.log('hsb', hsb);
+      return sk.color(hsb);
     });
   };
 
   const tweenColors = () => {
     if (counter >= 1) {
       counter = 0;
-      initBands();
-      return;
+      colors.shift();
+      colors.push(sk.color(getColorVariation(getMainColor())));
+      return colors;
     } 
-    colors.forEach((c, i, arr) => {
+
+    return colors.map((c1, i, arr) => {
+      // console.log('tween colors', counter);
       const c2 = arr[i + 1];
       if (!c2) {
         return;
       }
-      return sk.lerpColor(c, c2, counter);
+      console.log(c1, c2);
+      return sk.lerpColor(c1, c2, counter);
     });
-
   }
 
   sk.windowResized = function() {
@@ -44,16 +58,31 @@ new p5((sk) => {
     canvasWidth = sk.width;
   }
 
+  sk.mousePressed = function() {
+    console.log('pressed');
+  }
+
+  sk.mouseMoved = function() {
+    console.log('mmmmover');
+  }
+
   sk.draw = () => {
-    sk.push();
-    sk.translate(-canvasWidth/2, -canvasHeight/2);
+ 
     const bandHeight = canvasHeight/bandCount;
     counter += .01;
-    tweenColors();
+    sk.noStroke();
+    sk.push();
+    sk.translate(-canvasWidth/2, -canvasHeight/2);
+   
+    const newColors = tweenColors();
     for (let b = 0; b < bandCount; b++) {
-      sk.fill(colors[b]);
+      sk.fill(newColors[b]);
       sk.rect(0, bandHeight * b, canvasWidth, bandHeight);
     }
     sk.pop();
+
+    if (counter >= 1) {
+      counter = 0;
+    }
   };
 });
