ddcebf284deb96bb5deffb24bb226f601d14d27a
ivy <unjust@gmail.com>
2020-06-14 19:17:35 -0500

    more viusals

---
+++ js/differentForms.p5.js
@@ -0,0 +1,106 @@
+import p5 from 'p5';
+import { drawCoordinates } from 'Utils/coordinates';
+import { createEasyCam } from 'Libraries/easycam/p5.easycam.js';
+import { 
+  getStepVertices,
+  getContinuousVertices,
+  drawVertices,
+  selectNextRandomVertex
+} from 'Framework/verticesHelpers';
+
+new p5((sk) => {
+
+  let counter = 1, // start off at 1 to selectVertices
+    step = 1;
+  
+  // a buffer for the current shape
+  let drawingBuffer = [],
+    allShapesBuffers = []; // a buffer for all shapes
+
+  let verticesToConnect = [];
+  let selectedVertex = 0;
+
+  sk.setup = () => {
+    sk.createCanvas(600, 500, sk.WEBGL);
+    sk.background(255);
+    drawCoordinates(sk);
+    
+    (createEasyCam.bind(sk))();
+    console.log("same as steppedDrawing but organized code slightly differently")
+  };
+
+
+  const resetBuffers = () => {
+    // out of vertices, push the shape to the buffer
+    allShapesBuffers.push(drawingBuffer);
+    drawingBuffer = [];
+  }
+
+  const getShapeVerticesComplex = () => {
+    // const howManyVertices = sk.random(3, 10);
+    // some random or patterned mix of solid and step
+
+    // reset the buffer process
+    verticesToConnect = getStepVertices(getSelectedVertices());
+    selectedVertex = 0;
+  }
+
+  const getShapeVertices = () => {
+    const prev = verticesToConnect[verticesToConnect.length - 1];
+    const nextVertex = selectNextRandomVertex(
+      prev,
+      120, // make this controlled by input somehow
+      true,
+      sk);
+
+    // seed if no vertices
+    if (!verticesToConnect.length) {
+      verticesToConnect.push(nextVertex);
+      verticesToConnect.push(selectNextRandomVertex(
+        nextVertex,
+        120, 
+        true,
+        sk));
+      return;
+    }
+   
+    if (sk.random(10) % 2) { // odd
+      verticesToConnect.push(...getStepVertices([prev, nextVertex]));
+    } else {
+      verticesToConnect.push(nextVertex);
+    }
+  }
+
+  sk.draw = () => {
+    sk.strokeWeight(2);
+    sk.stroke(0);
+    sk.noFill();
+    sk.background(255);
+   
+    drawBuffers();
+  };
+
+  const drawBuffers = function() {
+    // push new vertices
+    if (counter < 1) {
+      drawingBuffer.push(
+        getContinuousVertices(verticesToConnect[selectedVertex], verticesToConnect[selectedVertex+1], counter)
+      );
+      counter += step;
+    } else { // if counter is at 100%
+       // increment the selected vertex, if we havent run out
+      if (selectedVertex < verticesToConnect.length - 1) {
+        selectedVertex++;
+        getShapeVertices();
+      } else {
+        resetBuffers();
+        getShapeVertices();
+      }
+      counter = 0;
+    }
+    
+    // draw
+    allShapesBuffers.forEach((vArr) => drawVertices(vArr, sk));
+    drawVertices(drawingBuffer, sk);
+  }
+});
---
+++ js/triangle_rotation_random.p5.js
@@ -0,0 +1,124 @@
+import p5 from 'p5';
+import { createEasyCam } from 'Libraries/easycam/p5.easycam.js';
+
+new p5((sk) => {
+
+  const DIM = 20;
+  let tw = DIM * 2;
+  let th = Math.sqrt((tw*tw) - (tw*tw/4));
+
+  let triangles = [];
+  const indexesToDraw = [];
+  let rAxis, indexes;
+
+  // let counter = 0;
+  
+  sk.setup = () => {
+    rAxis = sk.createVector(0, 1, 0);
+    sk.createCanvas(1000, 800, sk.WEBGL);
+    sk.createTriangles();
+    sk.pickTrianglesToDraw();
+    createEasyCam.bind(sk)();
+  };
+
+  sk.createTriangles = function() {
+
+    for (let x = 0; x < sk.width + tw; x += tw) {
+      for (let y = 0; y < sk.height + th; y += th) {
+
+        const triangle1 = {
+          x1: x,
+          y1: y,
+          x2: x + tw/2,
+          y2: y + th,
+          x3: x + tw,
+          y3: y,
+          counter: 0,
+          inMotion: false
+        };
+
+        const triangle2 = {
+          x1: x + tw/2,
+          y1: y + th,
+          x2: x + tw,
+          y2: y,
+          x3: x + tw + tw/2,
+          y3: y + th,
+          counter: 0,
+          inMotion: false
+        };
+
+        triangles.push(triangle1, triangle2)
+      }
+    }
+    indexes = Array.from(Array(triangles.length).keys());
+    console.log(`${triangles.length} triangles created!`);
+  };
+
+  sk.pickTrianglesToDraw = () => {
+    let triangleAmount = 15;
+ 
+    while (triangleAmount > 0) {
+      indexesToDraw.push(Math.floor(sk.random(0, triangles.length)));
+      triangleAmount--;
+    }
+
+  }
+
+  const toggleCursor = () => {
+    document.body.className == "noCursor" ? 
+      document.body.className = "" : document.body.className = "noCursor";
+  }
+
+  sk.keyPressed = () => {
+    toggleCursor();  
+  };
+
+  sk.draw = () => {
+    /* glitching because changing the animation
+    if (sk.keyPressed()) {} 
+    */
+    
+    // pick an index
+    const triangleIndex = sk.random(indexes);
+    indexes.splice(triangleIndex, 1);
+
+    // store it
+    // move the triangle
+
+    sk.clear();
+    sk.background(0);
+    sk.fill(253, 246, 227);
+    sk.translate(-sk.width/2, -sk.height/2, 0);
+
+    // const millis = sk.millis();
+    // counter += .001;
+
+    // around axis
+    const half_screen = sk.width/2;
+    
+    triangles.forEach((t, i) => {
+      sk.stroke(253, 246, 227);
+      
+      if (indexes.length && i === triangleIndex) {
+      // if (indexesToDraw.indexOf(i) !== -1) {
+        t.inMotion = true;
+      }
+      if (t.inMotion) {
+        t.counter += .001;
+        sk.stroke(221, 214, 193);
+      }
+
+      sk.push();
+      sk.beginShape();
+      sk.translate(sk.width/2, 0);
+      sk.rotate(t.counter * (i % 20), rAxis);
+      sk.vertex(t.x1 - half_screen, t.y1);
+      sk.vertex(t.x2 - half_screen, t.y2);
+      sk.vertex(t.x3 - half_screen, t.y3);
+      sk.endShape(sk.CLOSE);
+      sk.pop();
+    });
+  };
+}, 'container');
+
