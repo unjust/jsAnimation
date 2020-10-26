02fa880e02cc2b8b9a18815a829f51fb3e83d148
ivy <unjust@gmail.com>
2020-10-25 20:22:35 -0500

    fft and midi in soundtools, vertexBuffer

--- js/myLib/soundTools.js
+++ js/myLib/soundTools.js
@@ -1,20 +1,22 @@
 import p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
+// TODO use https://github.com/djipco/webmidi ??
 
 let audio = null;
 let fft = null;
+let midiMessageCallback = () => {};
 
 export const initAudioIn = (labelName="iShowU Audio Capture") => {
   audio = new p5.AudioIn((e) => console.log("error starting audio in"));
   audio.start();
   audio.getSources().
     then((sourceList) => { 
-      console.table(sourceList);
+      // console.table(sourceList);
       const newSrcIndex = sourceList.findIndex((input) => input.label.includes(labelName));
       audio.setSource(newSrcIndex);
       setFFTInputSrc();
     });
-  console.log(audio.stream, audio.currentSource);
+  // console.log(audio.stream, audio.currentSource);
   return audio;
 }
 
@@ -28,3 +30,39 @@ export const initFFT = (bins=1024, smoothing) => {
 }
 
 export const getSpectrum = () => fft.analyze();
+
+export function onMIDIMessage(e) {
+  const message = e.data;
+  // console.log(message, e);
+
+  // [176, 5, 63]
+  const command = message[0];
+  const note = message[1];
+  const velocity = message[2] || 0; 
+
+  // https://www.smashingmagazine.com/2018/03/web-midi-api/
+  // This array typically contains three values (e.g. [144, 72, 64]). 
+  // The first value tells us what type of command was sent, the second is the note value, and the third is velocity. The command type could be either “note on,” “note off,” 
+  midiMessageCallback(velocity, command, note);
+  // console.log(velocity);
+}
+
+function onMIDISuccess(midiAccess) {
+  const inputs = midiAccess.inputs;
+  // const outputs = midiAccess.outputs;
+
+  // for (var input of midiAccess.inputs.values())
+  //  input.onmidimessage = getMIDIMessage;
+  // }
+  inputs.values().next().value.onmidimessage = onMIDIMessage;
+}
+
+function onMIDIFailure() {
+  console.log('Could not access your MIDI devices.');
+}
+
+export const initMIDI = (callback=()=>{}) => {
+  midiMessageCallback = callback;
+  navigator.requestMIDIAccess()
+    .then(onMIDISuccess, onMIDIFailure);
+}
--- js/texture.p5.js
+++ js/texture.p5.js
@@ -1,54 +1,79 @@
 import p5 from 'p5';
-import {  initFFT, getSpectrum, initAudioIn } from 'Framework/soundTools';
+import {  initFFT, getSpectrum, initAudioIn, initMIDI } from 'Framework/soundTools';
 
 new p5((sk) => {
+
+  let midiValue;
+  const setMidiValue = (value) => midiValue = value;
+  const bins = 1024;
   sk.setup = () => {
     sk.pixelDensity(1);
     sk.createCanvas(1000, 500, sk.WEBGL);
     sk.background('#c6c1b8');
-    // sk.stroke('black') 
+    sk.stroke('black') 
+    sk.strokeWeight(1);
+    sk.noFill();
     initAudioIn();
-    
-    initFFT(1024);
+    initFFT(bins);
+    initMIDI(setMidiValue);
   };
 
-  const counter = () => sk.millis() / 1000;
+  
   const noiseScale = 0.02;
+
+  const vertexBuffer = new Array(bins);
+
+  const fillVertexBuffer = (newArray) => {
+    const len = vertexBuffer.length;
+    const firstValue = vertexBuffer.shift();
+    vertexBuffer[len - 1] = newArray;
+    return firstValue || [];
+  } 
+
+  const counter = () => sk.millis() / 1000;
+  const XY = () => Math.sin(sk.millis()/100000)*1000;
 
   const drawLine = (x1, x2, y) => {
     const VERTS = 512;
     const dist = (x2 - x1) / VERTS;
-    sk.beginShape(sk.LINES);
-    const spectrum = getSpectrum(VERTS);
+    sk.beginShape(sk.TRIANGLE_STRIP);
+    // const spectrum = getSpectrum(VERTS);
+
+    const buffer0 = fillVertexBuffer([...new Array(VERTS)].map((v=0, i) => {
+      let noiseVal = sk.noise((XY() + i) * noiseScale, XY() * noiseScale) * 100;
+      return y + noiseVal;
+    }));
+
     [...new Array(VERTS)].forEach((v=0, i) => {
       // console.log(`vertex ${i}`, noiseVal);
-      let noiseVal = sk.noise((sk.mouseX + i) * noiseScale, sk.mouseY * noiseScale) * 100;
-      sk.vertex(x1 + (i * dist), y + spectrum[i]);
+      // sk.vertex(x1 + (i * dist), y + spectrum[i]);
+      // sk.vertex(x1 + (i * dist), y + noiseVal * midiValue/10, 0);
+      sk.vertex(x1 + (i * dist), buffer0[i], 0);
     });
     sk.endShape();
   };
 
-  const drawLines = (x1, x2, y) => [...new Array(1)].forEach((v, i) => drawLine(x1, x2, 20 * i));
+  const drawLines = (x1, x2, y) => [...new Array(27)].forEach((v, i) => drawLine(x1, x2, -sk.canvas.height/2 + 20 * i));
 
   sk.draw = () => {
     sk.clear();
     sk.background('#c6c1b8');
+  
     // let spectrum = getSpectrum(512);
     // console.log(spectrum);
     // for every block of width 
     // canvas width / BLOCKS = width next
 
-    const BLOCKS = 2;
-    const WIDTH = sk.canvas.width/BLOCKS;
-    for (let i = 0; i < BLOCKS; i++) {
-      sk.push();
-      sk.rotateX(counter() / (i/20 + 1));
-      sk.stroke(0);
-      const x1 = -sk.canvas.width/2 + (WIDTH * i);
-      const x2 =  x1 + WIDTH;
-      drawLines(x1, x2);
-      sk.pop();
-    }
-    // drawLines(-sk.canvas.width/2 + 50, sk.canvas.width/2 - 100);
+    // const BLOCKS = midiValue || 1;
+    // const WIDTH = sk.canvas.width/BLOCKS;
+    // for (let i = 0; i < BLOCKS; i++) {
+    //   sk.push();
+    //   sk.rotateX(counter() / (i/20 + 1));
+    //   const x1 = -sk.canvas.width/2 + (WIDTH * i);
+    //   const x2 =  x1 + WIDTH;
+    //   drawLines(x1, x2);
+    //   sk.pop();
+    // }
+    drawLines(-sk.canvas.width/2 + 50, sk.canvas.width/2 - 100);
   };
 });
