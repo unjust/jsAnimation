5e507f113628c1dc7ffb96e01e8a73429d98aff1
ivy <unjust@gmail.com>
2021-10-23 22:02:18 -0500

    doc touchevents

--- js/myLib/mixins/touchEvents.js
+++ js/myLib/mixins/touchEvents.js
@@ -1,16 +1,35 @@
 // https://github.com/freshfork/p5.EasyCam/blob/master/p5.easycam.js
-/*
+/**
+@description
+override/extend events to support touch
+in particular projects
+
+@example
+easycam = createEasyCam.bind(sk)();
+extendTouchEasycam(easycam, {
+  dbltap: (e) => {
+    sk.keyPressed(e);
+    sk.keyIsPressed = !sk.keyIsPressed;
+  },
+  // touchmoveMulti: (e) => {
+  //   sk.keyIsPressed = true;
+  // }
+})
+
+@events
+in easycam:
 'touchstart'
 'dbltap'
 'touchmoveSingle'
 'touchmoveMulti'
-*/
+**/
+
 export const extendTouchEasycam = function(easycam, opts) {
   for (let prop in opts) {
     if (easycam.mouse[prop]) {
       easycam.mouse[prop] = opts[prop];
     }
   }
-  easycam.removeMouseListeners();
+  easycam.removeMouseListeners(); // remove what has been defined and reattach overrides
   easycam.attachMouseListeners();
 } 
