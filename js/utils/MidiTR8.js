387bbb01648d6d8145d71c53c842b4ce14377192
ivy <unjust@gmail.com>
2022-05-22 17:52:58 -0500

    adding TR8 support

---
+++ js/utils/MidiTR8.js
@@ -0,0 +1,89 @@
+import { accessMIDI } from "Utils/Midi.js"
+
+
+// 137, 153 ch 10 note on/off 
+// https://www.midi.org/specifications-old/item/table-2-expanded-messages-list-status-bytes
+const TR8_keys = {
+ 36: 'BD',
+ 38: 'SD',
+ 43: 'LT',
+ 47: 'MT',
+ 50: 'HT',
+ 37: 'RS',
+ 39: 'HC',
+ 42: 'CH',
+ 46: 'OH',
+ 49: 'CC',
+ 51: 'RC',
+}
+
+const instrumentHandlerDefaults = {
+  onBD: function() {},
+  onSD: function() {},
+  onLT: function() {},
+  onMT: function() {},
+  onHT: function() {},
+  onRS: function() {},
+  onHC: function() {},
+  onCH: function() {},
+  onOH: function() {},
+  onCC: function() {},
+  onRC: function() {},
+}
+const noteQueue = [];
+let onMidiHandler = () => {};
+let onNoteOnHandler = () => {};
+let onNoteOffHandler = () => {};
+let onControlChangeHandler = () => {};
+let instrumentHandlers = instrumentHandlerDefaults;
+
+export const init = ({ onMidiFn=()=>{},
+    onNoteOn=()=>{},
+    onNoteOff=()=>{},
+    onControlChange=()=>{},
+    instHandlers={}
+  }) => {
+  // 185 is control change channel 10
+  onMidiHandler = onMidiFn;
+  onNoteOnHandler = onNoteOn;
+  onNoteOffHandler = onNoteOff;
+  onControlChangeHandler = onControlChange;
+  instrumentHandlers = { ...instrumentHandlerDefaults, ...instHandlers };
+  accessMIDI(_onMidi);
+}
+
+const _onMidi = function(msg) {
+  const [ type, key, velocity ] = msg.data;
+  if (!key) {
+    return;
+  }
+  
+  // console.log(msg.data)
+  // https://code.tutsplus.com/tutorials/introduction-to-web-midi--cms-25220
+  // 153 is note on, 137 is not off channel 10
+  // 176 - 191 is control change 185 ch 10 https://www.midi.org/specifications-old/item/table-2-expanded-messages-list-status-bytes
+  // 201 ch 10 program change, 192 ch 1 program change
+
+  const instHandlerName = `on${TR8_keys[key]}`;
+  // all values are for channel 10
+  switch(type) {
+    case 137:
+      // ch 10 note off
+      onNoteOffHandler(key);
+      
+      instrumentHandlers[instHandlerName]("noteoff", velocity);
+      break;
+    case 153:
+      noteQueue.push(key);
+      onNoteOnHandler(key);
+      instrumentHandlers[instHandlerName]("noteon", velocity);
+      break;
+    case 185:
+      onControlChangeHandler(key); // is key or velocity right here?
+    default:
+      break;
+  }
+}
+
+
+
