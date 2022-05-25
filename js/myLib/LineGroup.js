6684d1d9a92d393fe62ad38eff962fa426c01c95
ivy <unjust@gmail.com>
2022-05-24 21:32:51 -0500

    adding xy radius and keep drawing

--- js/myLib/LineGroup.js
+++ js/myLib/LineGroup.js
@@ -2,34 +2,36 @@ export class LineGroup {}
 
 export class CurvedLineGroup {
 
-  constructor({ len=60, centerX=0, centerY=0, sk }) {
+  constructor({ len=60, centerX=0, centerY=0, xRadius=200, yRadius=200, sk }) {
     this.len = len;
     this.buffer = Array.from(Array(len), () => ({ x: 0, y: 0 }));
     this.newestLine = 0;
     this.sk = sk;
     this.centerX = centerX;
     this.centerY = centerY;
+    this.xRadius = xRadius;
+    this.yRadius = yRadius;
   }
   
-  draw() {
+  draw(keepDrawing) {
     const sk = this.sk;
 
     const t = sk.millis()/500;
-    // const scale = 2 / (3 - Math.cos(2*t));
-
     const bufferLen = this.buffer.length;
 
     // https://gamedev.stackexchange.com/questions/43691/how-can-i-move-an-object-in-an-infinity-or-figure-8-trajectory
     this.newestLine = sk.frameCount % bufferLen;
       
-    // if (drawLine) {
-    this.buffer[this.newestLine] = { x: 200 + 200 * Math.cos(t), y: 300 + 200 * Math.sin(2*t) / 2 };
-    // } else {
-    //   lineBuffer[newestLine] = undefined;
-    // }
+    if (keepDrawing) {
+      this.buffer[this.newestLine] = { x: this.centerX + this.xRadius * Math.cos(t), y: this.centerY + this.yRadius * Math.sin(2*t) / 2 };
+                                // { x: 200 + 200 * Math.cos(t), y: 300 + 200 * Math.sin(2*t) / 2 };
+    }
+    else {
+      this.buffer[this.newestLine] = undefined;
+    }
 
     sk.push();
-    sk.translate(-sk.width/2 + this.centerX, -sk.height/2 + this.centerY);
+    sk.translate(-sk.width/2, -sk.height/2);
 
     sk.fill(255, 153);
     sk.stroke(255, 153);
@@ -39,6 +41,7 @@ export class CurvedLineGroup {
       const xy = this.buffer[index];
       // sk.ellipse(xy.x, xy.y, i, i);
       if (xy) {
+        // console.log(xy);
         sk.line(xy.x, xy.y, xy.x + bufferLen - 1, xy.y + bufferLen - i);
       }
     }
